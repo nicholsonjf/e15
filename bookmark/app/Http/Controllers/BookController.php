0289591e4d11726d4718d7ac37830480e7f86e82
James Nicholson <jnicholson@hsph.harvard.edu>
2020-03-03 10:10:05 -0500

    Initializing p2 and some bookmark changes

---
+++ bookmark/app/Http/Controllers/BookController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+    * GET /books
+    */
+    public function index()
+    {
+        # Work that was previously happening in the routes file is now happening here
+        return 'Here are all the books...';
+    }
+
+    /**
+    * GET /books/{title}
+    */
+    public function show($title)
+    {
+        if ( $title === '' ) {
+            return 'duhhhh where is the book?';
+        }
+        else {
+            return 'Here is your book: ' . $title;
+        }
+    }
+}
--- p2/routes/web.php
+++ p2/routes/web.php
@@ -11,19 +11,10 @@
 |
 */
 
+Route::get('/', function () {
+    return 'Versia A';
+});
 
 Route::get('/', function () {
-    return 'Here for the books?';
-});
-
-Route::get('/books', 'BookController@index');
-
-Route::get('/books/{title}', 'BookController@show');
-
-Route::get('/filter/{category}/{subcategory}', function($category, $subcategory) {
-    return 'Here are all the books in the category '.$category.' and '.$subcategory;
-});
-
-Route::get('/example', function () {
-    return 'example...';
+    return 'Versia B';
 });
