d7930828f565c7ffc06b1917c3add1d71b08c01c
James Nicholson <jnicholson@hsph.harvard.edu>
2020-03-29 21:26:22 -0400

    form data repopulates after submit

--- p2/app/Http/Controllers/PopulationController.php
+++ p2/app/Http/Controllers/PopulationController.php
@@ -14,18 +14,25 @@ class PopulationController extends Controller
     {
         $populations_file = Storage::disk('local')->get('world-population-2019.json');
         $populations_data = json_decode($populations_file, true);
-        return view('population.index')->with(['populations' => $populations_data]);
+        return view('population.index')->with([
+            'populations' => $populations_data,
+            'difficulty' => session('difficulty', null),
+            'country' => session('country', null),
+            'guess' => session('guess', null)
+            ]);
     }
 
     public function guess(Request $request)
     {
-        $difficulty = intval($request->all()['difficulty']);
-        $percentage = $difficulty / 100
-        $guess = intval($request->all()['guess']);
-        dd($difficulty);
-        $populations_file = Storage::disk('local')->get('world-population-2019.json');
+        $difficulty = intval($request->input('difficulty'));
+        $percentage = $difficulty / 100;
+        $guess = intval($request->input('guess'));
+        $populations_file = Storage::disk('local')->get('world-population-2019-keyed.json');
         $populations_data = json_decode($populations_file, true);
-        dd($populations_data);
-        return 'you guessed it!';
+        return redirect('/population')->with([
+            'difficulty' => $request->input('difficulty'),
+            'guess' => $request->input('guess'),
+            'country' => $request->input('country')
+        ]);
     }
 }
--- p2/resources/views/population/index.blade.php
+++ p2/resources/views/population/index.blade.php
@@ -5,14 +5,19 @@
 @endsection
 
 @section('content')
-<form action="{{ url('population/guess')  }}" method="GET">
+<form action='/population/guess' method='GET'>
     <div>
         <h3>Country</h3>
-        <label for="country-select">Choose a country:</label>
-        <select name="country" id="country-select">
-            <option value="">--Please choose an option--</option>
+        <label for='country-select'>Choose a country:</label>
+        <select name='country' id='country-select'>
+            <option value=''>--Please choose an option--</option>
             @foreach($populations as $population)
-                <option value={{$population['country']}}>{{$population['country']}}</option>
+                <option
+                    value='{{ trim($population['country']) }}'
+                    {{ ($country == trim($population['country']) ) ? ' selected' : '' }}
+                >
+                    {{ $population['country'] }}
+                </option>
             @endforeach
         </select>
     </div>
@@ -21,24 +26,48 @@
         <h3>Difficulty</h3>
         <p>Set your difficulty. If you select 5%, your guess must be within 5% of that country's population, and so on...</p>
 
-        <input type="radio" id="difficulty5" name="difficulty" value="5">
-        <label for="difficulty5">5%</label>
+        <input
+            type='radio'
+            id='difficulty5'
+            name='difficulty'
+            value='5'
+            {{ $difficulty == '5' || is_null($difficulty) ? ' checked' : ''}}
+        >
+        <label for='difficulty5'>5%</label>
 
-        <input type="radio" id="difficulty10" name="difficulty" value="10">
-        <label for="difficulty10">10%</label>
+        <input
+            type='radio'
+            id='difficulty10'
+            name='difficulty'
+            value='10'
+            {{ $difficulty == '10' ? ' checked' : ''}}
+        >
+        <label for='difficulty10'>10%</label>
 
-        <input type="radio" id="difficulty25" name="difficulty" value="25">
-        <label for="difficulty25">25%</label>
+        <input
+            type='radio'
+            id='difficulty25'
+            name='difficulty'
+            value='25'
+            {{ $difficulty == '25' ? ' checked' : ''}}
+        >
+        <label for='difficulty25'>25%</label>
     </div>
 
     <div>
         <h3>Guess</h3>
-        <label for="guess">Guess the population:</label>
-        <input type="text" id="guess" name="guess">
+        <label for='guess'>Guess the population:</label>
+        <input
+            type='text'
+            id='guess'
+            name='guess'
+            value='{{ $guess }}'
+        >
     </div>
 
     <div>
-        <button type="submit">Submit</button>
+        <button type='submit'>Submit</button>
     </div>
 </form>
+
 @endsection
