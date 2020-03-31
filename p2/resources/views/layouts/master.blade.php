c7e576fc675fc95b7f3824e9b11b1c594c67705f
James Nicholson <jnicholson@hsph.harvard.edu>
2020-03-31 00:45:11 -0400

    Styling is good to go

--- p2/resources/views/layouts/master.blade.php
+++ p2/resources/views/layouts/master.blade.php
@@ -10,19 +10,23 @@
     @yield('head')
 </head>
 <body>
-    <div class='container'>
-        <h1>Population Guesser!</h1>
-        <header>
-            <a href='/'><img src='/images/earth.png' id='logo' alt='Population Logo'></a>
-        </header>
+    <div class='container-fluid'>
+        <div class="row flex-xl-nowrap">
+            <div class="col-12 col-md-3 col-xl-2 bd-sidebar"></div>
+            <main class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content" role="main">
+                <h1>The Population Game!</h1>
+                <header>
+                    <a href='/'><img src='/images/world.jpg' id='logo' alt='Population Logo' class='rounded-sm'></a>
+                </header>
 
-        <section>
-            @yield('content')
-        </section>
-
-        <footer>
-            &copy; {{ date('Y') }}
-        </footer>
+                <section>
+                    @yield('content')
+                </section>
+                <footer>
+                    &copy; {{ date('Y') }}
+                </footer>
+            </main>
+        </div>
     </div>
 </body>
 </html>
--- p2/resources/views/population/index.blade.php
+++ p2/resources/views/population/index.blade.php
@@ -13,71 +13,86 @@
     </ul>
 @endif
 <form action='/population/guess' method='GET'>
-    <div class='form-group'>
-        <h3>Country</h3>
-        <label for='country-select'>Choose a country:</label>
-        <select name='country' id='country-select' class='form-control'>
-            <option value=''>--Please choose an option--</option>
-            @foreach($populations as $population)
-                <option
-                    value='{{ trim($population['country']) }}'
-                    {{ ($country == trim($population['country']) ) ? ' selected' : '' }}
+    <div class='row py-4'>
+        <div class='col'>
+            <h3>Country</h3>
+            <div class='form-group'>
+                <label for='country-select'>Choose a country:</label>
+                <select name='country' id='country-select' class='form-control'>
+                    <option value=''>--Please choose an option--</option>
+                    @foreach($populations as $population)
+                        <option
+                            value='{{ trim($population['country']) }}'
+                            {{ ($country == trim($population['country']) ) ? ' selected' : '' }}
+                        >
+                            {{ $population['country'] }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+    </div>
+
+    <div class='row pb-4'>
+        <div class='col'>
+            <h3>Difficulty Level</h3>
+            <p>Set your difficulty. If you select "Hard", your guess must be within 5% of that country's population, and so on...</p>
+            <div class='form-check'>
+                <input
+                    class='form-check-input'
+                    type='radio'
+                    id='difficulty5'
+                    name='difficulty'
+                    value='5'
+                    {{ $difficulty == '5' || is_null($difficulty) ? ' checked' : ''}}
                 >
-                    {{ $population['country'] }}
-                </option>
-            @endforeach
-        </select>
+                <label for='difficulty5' class='form-check-label'>Hard</label>
+            </div>
+            <div class='form-check'>
+                <input
+                    class='form-check-input'
+                    type='radio'
+                    id='difficulty10'
+                    name='difficulty'
+                    value='10'
+                    {{ $difficulty == '10' ? ' checked' : ''}}
+                >
+                <label for='difficulty10' class='form-check-label'>Medium</label>
+            </div>
+            <div class='form-check'>
+                <input
+                    class='form-check-input'
+                    type='radio'
+                    id='difficulty25'
+                    name='difficulty'
+                    value='25'
+                    {{ $difficulty == '25' ? ' checked' : ''}}
+                >
+                <label for='difficulty25' class='form-check-label'>Easy</label>
+            </div>
+        </div>
     </div>
 
-    <div class='form-check'>
-        <h3>Difficulty</h3>
-        <p>Set your difficulty. If you select 5%, your guess must be within 5% of that country's population, and so on...</p>
-
-        <input
-            class='form-check-input'
-            type='radio'
-            id='difficulty5'
-            name='difficulty'
-            value='5'
-            {{ $difficulty == '5' || is_null($difficulty) ? ' checked' : ''}}
-        >
-        <label for='difficulty5' class='form-check-label'>5%</label>
-
-        <input
-            class='form-check-input'
-            type='radio'
-            id='difficulty10'
-            name='difficulty'
-            value='10'
-            {{ $difficulty == '10' ? ' checked' : ''}}
-        >
-        <label for='difficulty10' class='form-check-label'>10%</label>
-
-        <input
-            class='form-check-input'
-            type='radio'
-            id='difficulty25'
-            name='difficulty'
-            value='25'
-            {{ $difficulty == '25' ? ' checked' : ''}}
-        >
-        <label for='difficulty25' class='form-check-label'>25%</label>
+    <div class='row pb-4'>
+        <div class='col'>
+            <div class='form-group'>
+                <h3>Your Guess</h3>
+                <label for='guess'>Guess the population:</label>
+                <input
+                    class='form-control'
+                    type='text'
+                    id='guess'
+                    name='guess'
+                    value='{{ $guess }}'
+                >
+            </div>
+        </div>
     </div>
 
-    <div class='form-group'>
-        <h3>Guess</h3>
-        <label for='guess'>Guess the population:</label>
-        <input
-            class='form-control'
-            type='text'
-            id='guess'
-            name='guess'
-            value='{{ $guess }}'
-        >
-    </div>
-
-    <div class='form-group'>
-        <button type='submit' class='form-control'>Submit</button>
+    <div class='row pb-4'>
+        <div class='col'>
+            <button type='submit' class='btn btn-primary'>Submit</button>
+        </div>
     </div>
 </form>
 
