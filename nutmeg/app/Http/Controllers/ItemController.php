519b6e5da1496eaacad32112673fa443a0fb5b90
James Nicholson <jnicholson@hsph.harvard.edu>
2020-05-04 12:13:15 -0400

    Nutmeg items import transaction fails

--- nutmeg/app/Http/Controllers/ItemController.php
+++ nutmeg/app/Http/Controllers/ItemController.php
@@ -3,21 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use App\Item;
 use Storage;
 
+/**
+ * ItemController class
+ */
 class ItemController extends Controller
 {
     /**
      * Import items from csv file.
      *
-     * @param  string  $file_path
-     * @return $transaction_status
+     * @param string  $file_path Path to the file containing the data.
+     *
+     * @return $transaction
      */
     public function import($file_path)
     {
         $import_file = Storage::disk('local')->get($file_path);
-        $items = json_decode($import_file, true);
-        return $import_file;
+        $items = explode($import_file, '\n');
+        $transaction = DB::transaction(
+            function () use ($items) {
+                foreach ($items as $item) {
+                    DB::table('items')->insert(
+                        ['name' => $item[0]]
+                    );
+                }
+            }
+        );
+        return $transaction;
     }
 }
