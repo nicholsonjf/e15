6b59e4bb5b63482fcd529abf07f9d21ec18d6ad4
James Nicholson <jnicholson@hsph.harvard.edu>
2020-05-05 00:40:10 -0400

    Nutmeg items validated and imported

--- nutmeg/app/Http/Controllers/ItemController.php
+++ nutmeg/app/Http/Controllers/ItemController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use App\Item;
 use Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 /**
  * ItemController class
@@ -22,16 +24,41 @@ class ItemController extends Controller
     public function import($file_path)
     {
         $import_file = Storage::disk('local')->get($file_path);
-        $items = explode($import_file, '\n');
-        $transaction = DB::transaction(
-            function () use ($items) {
-                foreach ($items as $item) {
-                    DB::table('items')->insert(
-                        ['name' => $item[0]]
-                    );
-                }
+        $items_array = explode(PHP_EOL, $import_file);
+        $items_filtered = array_filter(
+            $items_array,
+            function ($line) {
+                return strlen($line) > 1;
             }
         );
-        return $transaction;
+        $items_assoc = array();
+        foreach ($items_filtered as $item_filtered) {
+            $items_assoc[] = array("name" => $item_filtered);
+        }
+        foreach ($items_assoc as $item_assoc) {
+            $validator = Validator::make(
+                $item_assoc,
+                [
+                    'name' => 'required|string',
+                ]
+            );
+            if ($validator->fails()) {
+                throw new Exception('nope');
+                //throw new ValidationException($validator);
+            }
+        }
+        DB::beginTransaction();
+        try {
+            foreach ($items_assoc as $item_validated) {
+                $item = new Item;
+                $item->name = $item_validated['name'];
+                $item->save();
+            }
+        } catch(\Exception $e) {
+            DB::rollback();
+            throw $e;
+        }
+        DB::commit();
+        return "Successfully inserted " . strval(count($items_assoc)) . " items.";
     }
 }
