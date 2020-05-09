646e54881b7a82d631f677a6fdfeca90a156a9c8
James Nicholson <jnicholson@hsph.harvard.edu>
2020-05-09 17:00:39 -0400

    ItemsTableSeeder working

--- nutmeg/database/seeds/ItemsTableSeeder.php
+++ nutmeg/database/seeds/ItemsTableSeeder.php
@@ -14,7 +14,7 @@ class ItemsTableSeeder extends Seeder
     public function run()
     {
         $itemController = app()->make('App\Http\Controllers\ItemController');
-        $items = $itemController->import($this->option('nutmeg-items.txt'));
+        $items = $itemController->import('nutmeg-items.txt');
         $count = count($items);
         $departments = App\Department::all();
         $department_keys = $departments->modelKeys();
@@ -22,9 +22,8 @@ class ItemsTableSeeder extends Seeder
             $item = new Item();
             $item->created_at = Carbon\Carbon::now()->subDays($count)->toDateTimeString();
             $item->updated_at = Carbon\Carbon::now()->subDays($count)->toDateTimeString();
-            $item->name = $itemData;
-            $item->department = $department_keys[rand(0, count($department_keys))];
-
+            $item->name = $itemData['name'];
+            $item->department_id = $department_keys[rand(0, count($department_keys) - 1)];
             $item->save();
 
             $count--;
